27f4b5c86a59c3fe987490f4e1130f295005371c
Basir <basir.jafarzadeh@gmail.com>
2019-03-27 21:18:20 +0430

    Updates

---
+++ precache-manifest.dcf1482635221626422a60d2050bf6a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0acd2d4291614b7037d3",
+    "url": "/ecommerce-shopping-cart/static/js/runtime~main.e982c798.js"
+  },
+  {
+    "revision": "7903e70fbf503b238ce6",
+    "url": "/ecommerce-shopping-cart/static/js/main.bbf6109d.chunk.js"
+  },
+  {
+    "revision": "dcf2fbb378d1a1d83557",
+    "url": "/ecommerce-shopping-cart/static/js/2.1a694377.chunk.js"
+  },
+  {
+    "revision": "7903e70fbf503b238ce6",
+    "url": "/ecommerce-shopping-cart/static/css/main.79fc3682.chunk.css"
+  },
+  {
+    "revision": "4546253ee78daf29e4b7f80f010d37ac",
+    "url": "/ecommerce-shopping-cart/index.html"
+  }
+];
