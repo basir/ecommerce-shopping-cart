47d60977acfc70ed88a9de3b8fb17dd1239bdd4d
Basir <basir.jafarzadeh@gmail.com>
2020-01-18 11:19:03 +0330

    add links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react';
-import Products from './components/Products';
-import Filter from './components/Filter';
-import Basket from './components/Basket';
+import React, { Component } from "react";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Basket from "./components/Basket";
 
-import './App.css';
+import "./App.css";
+import Copyright from "./components/Copyright";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { size: '', sort: '', cartItems: [], products: [], filteredProducts: [] };
+    this.state = {
+      size: "",
+      sort: "",
+      cartItems: [],
+      products: [],
+      filteredProducts: []
+    };
   }
   componentWillMount() {
-
-    if (localStorage.getItem('cartItems')) {
-      this.setState({ cartItems: JSON.parse(localStorage.getItem('cartItems')) });
+    if (localStorage.getItem("cartItems")) {
+      this.setState({
+        cartItems: JSON.parse(localStorage.getItem("cartItems"))
+      });
     }
 
-    fetch('http://localhost:8000/products').then(res => res.json())
-      .catch(err => fetch('db.json').then(res => res.json()).then(data => data.products))
+    fetch("http://localhost:8000/products")
+      .then(res => res.json())
+      .catch(err =>
+        fetch("db.json")
+          .then(res => res.json())
+          .then(data => data.products)
+      )
       .then(data => {
         this.setState({ products: data });
         this.listProducts();
@@ -27,10 +40,10 @@ class App extends Component {
   handleRemoveFromCart = (e, product) => {
     this.setState(state => {
       const cartItems = state.cartItems.filter(a => a.id !== product.id);
-      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { cartItems: cartItems };
-    })
-  }
+    });
+  };
 
   handleAddToCart = (e, product) => {
     this.setState(state => {
@@ -47,35 +60,44 @@ class App extends Component {
       if (!productAlreadyInCart) {
         cartItems.push({ ...product, count: 1 });
       }
-      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
       return { cartItems: cartItems };
     });
-  }
+  };
 
   listProducts = () => {
     this.setState(state => {
-      if (state.sort !== '') {
+      if (state.sort !== "") {
         state.products.sort((a, b) =>
-          (state.sort === 'lowestprice'
-            ? ((a.price > b.price) ? 1 : -1)
-            : ((a.price < b.price) ? 1 : -1)));
+          state.sort === "lowestprice"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : a.price < b.price
+            ? 1
+            : -1
+        );
       } else {
-        state.products.sort((a, b) => (a.id > b.id) ? 1 : -1);
+        state.products.sort((a, b) => (a.id > b.id ? 1 : -1));
       }
-      if (state.size !== '') {
-        return { filteredProducts: state.products.filter(a => a.availableSizes.indexOf(state.size.toUpperCase()) >= 0) };
+      if (state.size !== "") {
+        return {
+          filteredProducts: state.products.filter(
+            a => a.availableSizes.indexOf(state.size.toUpperCase()) >= 0
+          )
+        };
       }
       return { filteredProducts: state.products };
-    })
-  }
-  handleSortChange = (e) => {
+    });
+  };
+  handleSortChange = e => {
     this.setState({ sort: e.target.value });
     this.listProducts();
-  }
-  handleSizeChange = (e) => {
+  };
+  handleSizeChange = e => {
     this.setState({ size: e.target.value });
     this.listProducts();
-  }
+  };
 
   render() {
     return (
@@ -84,17 +106,25 @@ class App extends Component {
         <hr />
         <div className="row">
           <div className="col-md-9">
-            <Filter count={this.state.filteredProducts.length} handleSortChange={this.handleSortChange}
-              handleSizeChange={this.handleSizeChange} />
+            <Filter
+              count={this.state.filteredProducts.length}
+              handleSortChange={this.handleSortChange}
+              handleSizeChange={this.handleSizeChange}
+            />
             <hr />
-            <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+            <Products
+              products={this.state.filteredProducts}
+              handleAddToCart={this.handleAddToCart}
+            />
           </div>
           <div className="col-md-3">
-            <Basket cartItems={this.state.cartItems} handleRemoveFromCart={this.handleRemoveFromCart} />
+            <Basket
+              cartItems={this.state.cartItems}
+              handleRemoveFromCart={this.handleRemoveFromCart}
+            />
+            <Copyright />
           </div>
-
         </div>
-
       </div>
     );
   }
---
+++ src/components/Copyright.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+export default class Copyright extends Component {
+  render() {
+    return (
+      <div>
+        <h4>IMPORTANT LINKS</h4>
+        <ul>
+          <li>
+            <a href="https://github.com/basir/ecommerce-shopping-cart">
+              Download Source code{" "}
+            </a>
+          </li>
+          <li>
+            <a href="https://www.udemy.com/course/build-ecommerce-website-by-react-redux-in-one-hour/">
+              Watch Learning Video
+            </a>
+          </li>{" "}
+          <li>
+            <a href="https://codingwithbasir.com/course/learn-react-redux/">
+              Learn React From Scratch
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
