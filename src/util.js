db1833755f01fe747fa244547601f84f4fffe757
Ayush Garg <ayushgarg1810@gmail.com>
2021-12-02 05:25:44 +0530

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 export default {
     formatCurrency: function (num) {
-        return '$' + Number(num.toFixed(1)).toLocaleString() + ' ';
+        return '$' + (Number(num).toFixed(1)).toLocaleString() + ' ';
     }
 }
